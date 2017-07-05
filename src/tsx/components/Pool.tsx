6d677fa03ccb0721653620f55066630279e09aec
TimsManter <timsmanter@gmail.com>
2017-07-05 15:56:37 +0200

    Include task list

--- src/tsx/components/Pool.tsx
+++ src/tsx/components/Pool.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
+import { Well, Row } from "react-bootstrap";
+
+import Task from "./Task";
 
 export interface P { }
 export interface S { }
 
 export default class Pool extends React.Component<P, S> {
   render(): JSX.Element {
-    return null;
+    return (
+      <Row>
+        <Well className="step-pool">
+          <Task size={2}>Send Nudes</Task>
+          <Task>Send Nudes</Task>
+          <Task>Send Nudes</Task>
+          <Task>Send Nudes</Task>
+        </Well>
+      </Row>
+    );
   }
 }
