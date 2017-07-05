e4c4f6c4b4738a7115c68cfdc81ff4534bf62250
TimsManter <timsmanter@gmail.com>
2017-07-05 15:01:28 +0200

    Clean up Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -51,15 +51,6 @@ module.exports = {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  /*externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  },*/
-
   devServer: {
     contentBase: __dirname + "/dist"
   },
