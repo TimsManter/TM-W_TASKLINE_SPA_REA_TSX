ad961586fd337382b75474d8ca73b334e66369fa
TimsManter <timsmanter@gmail.com>
2017-07-05 14:59:23 +0200

    Update App.tsx file

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import Grid from "./components/Hello";
-
-ReactDOM.render(
-  <Grid compiler="TypeScript" framework="React" />,
-  document.getElementById("app")
-);
