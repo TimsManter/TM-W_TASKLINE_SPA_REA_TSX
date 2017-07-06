747546ece6e0fe22c15246954451d8c5eeda39b0
TimsManter <timsmanter@gmail.com>
2017-07-06 10:10:07 +0200

    Add context decorator

--- src/tsx/components/Pool.tsx
+++ src/tsx/components/Pool.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { Well, Row } from "react-bootstrap";
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
 import Task from "./Task";
 
 export interface P { }
 export interface S { }
 
+@DragDropContext(HTML5Backend)
 export default class Pool extends React.Component<P, S> {
-  render(): JSX.Element {
+  
+  render(): JSX.Element | false | null {
     return (
       <div>
         <Well className="step-pool">
